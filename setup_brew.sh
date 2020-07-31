26832bb4192fdf44b86a1ee5a95a850a3fcae92d
Aakash Prabhu <aakprabhu@ucdavis.edu>
2020-07-31 14:59:38 -0700

    brew: add few helper functions

---
+++ setup_brew.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+function install_tools () {
+    brew install python
+    brew install lua
+    brew install boost
+    brew install coreutils
+    brew install cmake
+    brew install ytop
+    brew install tree
+    brew install tldr
+    brew install tmux
+    brew install node
+    brew install highlight
+}
+
+function install_functional_apps () {
+    brew cask install cheatsheet
+    brew cask install docker
+    brew cask install dropbox
+    brew cask install mactex
+}
+
+function install_programming_apps () {
+    brew cask install iterm2
+    brew cask install atom
+    brew cask install docker
+}
+
+function install_chat_apps () {
+    brew cask install slack
+    brew cask install skype
+}
+
+function brew_install () {
+    # check if brew already exists
+    which -s brew
+    if [[ $? != 0 ]]; then
+        # Homebrew doesn't exist so install it
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    else
+        # Use the latest version of homebrew and upgrade any preexisting routines
+        brew udpate
+        brew upgrade
+    fi
+}
